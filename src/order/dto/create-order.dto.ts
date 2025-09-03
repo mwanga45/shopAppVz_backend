9652dc3e5e92eb8a407d68b07bcf0aab005ea6c4
mwanga45 <issamwanga02@gmail.com>
2025-09-04 00:34:11 +0300

    create orderDto

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,1 +1,19 @@
-export class CreateOrderDto {}
+import { isNotEmpty, IsString } from "class-validator";
+
+export class CreateOrderDto {
+    @IsString()
+    user_id:string
+
+    @IsString()
+    product_name:string
+
+    @IsString()
+    client_name:string
+
+    @IsString()
+    client_phone:string
+
+    @IsString()
+    OrderDate:string
+
+}
