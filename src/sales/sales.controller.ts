ef89383b4b9bd6076d09b4b20889f1e582ceed81
mwanga45 <issamwanga02@gmail.com>
2025-10-05 16:48:53 +0300

    more changes

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -7,24 +7,6 @@ import { AuthGuard } from '@nestjs/passport';
 export class SalesController {
   constructor(private readonly salesService: SalesService) {}
 
-  @UseGuards(AuthGuard('jwt'))
-  @Post('recwholesale')
-  async Wholesales(
-    @Request() req,
-    @Body() createSaleDto: CreateSaleDto,
-  ): Promise<any> {
-    const userId = req.user.userId; 
-    return this.salesService.Whole_Sales_Record(createSaleDto, userId);
-  }
-  
-  @UseGuards(AuthGuard('jwt'))
-  @Post("recretailsale")
-  async Retailsales(@Request() req, @Body()createreDto:CreateRetailsalesDto):Promise<any>{
-    const userId = await req.user.userId;
-    return this.salesService.Retail_Sale_Record(createreDto,userId)
-    
-  }
-
   @Post()
   create(@Body() createSaleDto: CreateSaleDto) {
     return this.salesService.create(createSaleDto);
