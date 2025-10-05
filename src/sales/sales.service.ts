a776d201779143bf9553f4dbec1798f4848618bf
mwanga45 <issamwanga02@gmail.com>
2025-10-05 23:29:25 +0300

    more changes

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from 'src/product/entities/product.entity';
 import { Stock } from 'src/stock/entities/stock.entity';
 import { RetailSales } from './entities/retailsale.entity';
+import { Product_discount } from 'src/product/entities/discount.entity';
 import { NotFoundException } from '@nestjs/common';
 import { ResponseType } from 'src/type/type.interface';
 
@@ -17,7 +18,9 @@ export class SalesService {
     @InjectRepository(Product) private readonly ProductRepository:Repository<Product>,
     @InjectRepository(WholeSales) private readonly WholesalesRepository:Repository<WholeSales>,
     @InjectRepository(RetailSales) private readonly RetailsalesRepository:Repository<RetailSales>,
-    @InjectRepository(Stock) private readonly Stockrepo:Repository<Stock>
+    @InjectRepository(Stock) private readonly Stockrepo:Repository<Stock>,
+    @InjectRepository(Product_discount) private  readonly ProductDiscrepo:Repository<Product_discount>
+
   ){}
 
    StockCheck = async(productId:string):Promise<ResponseType<any>>=>{
@@ -25,13 +28,21 @@ export class SalesService {
     const FindStock = await this.Stockrepo.createQueryBuilder('s')
     .leftJoin('s.product', 'p')
     .select('s.Total_stock', 'totalstock')
-    
-    
+    .where('p.id = :id', {id})
+    .getRawMany()
      return{
       message:"sucessfuly  return",
       success:true,
-      data:check_stock
+      data:FindStock
      }
+  }
+  CheckDiscountCalculate = async (productId):Promise<ResponseType<any>> =>{
+    const checkDisc =  await this.ProductDiscrepo.createQueryBuilder('d')
+    
+    return{
+      message:"Successfuly",
+      success:true
+    }
   }
   
 }
