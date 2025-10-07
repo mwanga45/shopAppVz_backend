09e2d60c3c07c0ce531c50580f70ea79f800dcb0
mwanga45 <issamwanga02@gmail.com>
2025-10-07 22:02:54 +0300

    more changes

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -12,201 +12,232 @@ import { ResponseType } from 'src/type/type.interface';
 import { StockStatus } from 'src/type/type.interface';
 import { DeviationInput } from 'src/type/type.interface';
 
-
 @Injectable()
 export class SalesService {
   constructor(
-    @InjectRepository(Product) private readonly ProductRepository:Repository<Product>,
-    @InjectRepository(WholeSales) private readonly WholesalesRepository:Repository<WholeSales>,
-    @InjectRepository(RetailSales) private readonly RetailsalesRepository:Repository<RetailSales>,
-    @InjectRepository(Stock) private readonly Stockrepo:Repository<Stock>,
-    @InjectRepository(Product_discount) private  readonly ProductDiscrepo:Repository<Product_discount>
+    @InjectRepository(Product)
+    private readonly ProductRepository: Repository<Product>,
+    @InjectRepository(WholeSales)
+    private readonly WholesalesRepository: Repository<WholeSales>,
+    @InjectRepository(RetailSales)
+    private readonly RetailsalesRepository: Repository<RetailSales>,
+    @InjectRepository(Stock) private readonly Stockrepo: Repository<Stock>,
+    @InjectRepository(Product_discount)
+    private readonly ProductDiscrepo: Repository<Product_discount>,
+  ) {}
 
-  ){}
-
-   StockCheck = async(id:number,productAmount:number):Promise<ResponseType<any>>=>{
+  StockCheck = async (
+    id: number,
+    productAmount: number,
+  ): Promise<ResponseType<any>> => {
     const FindStock = await this.Stockrepo.createQueryBuilder('s')
-    .leftJoin('s.product', 'p')
-    .select('s.Total_stock', 'totalstock')
-    .where('p.id = :id', {id})
-    .getRawOne()
-      if(!FindStock){
-        return{
-          message:"No such product",
-          success:false
-        }
-      }
-      if(FindStock.totalstock < productAmount){
-       const product_status  = StockStatus.NotEnough
-        return{
-          message:"Not enough product",
-          success:true,
-          data:{...FindStock, product_status}
-          
-        }
-      }
-    const product_status = StockStatus.Enough
-     return{
-      message:"sucessfuly  return",
-      success:true,
-      data:{...FindStock, product_status }
-     }
-  }
-  CheckDiscountCalculate = async (productId:number,productAmount:number):Promise<ResponseType<any>> =>{
-    const checkDisc =  await this.ProductDiscrepo.createQueryBuilder('d')
-    .leftJoin('d.product', 'p')
-    .select('d.percentageDiscaunt', 'percentageDiscaunt')
-    .addSelect('d.CashDiscount','CashDiscount')
-    .addSelect('d.Product_start_from', 'start_from') 
-    .where('p.id = :productId', {productId})
-    .groupBy('p.id')
-    .addGroupBy('d.percentageDiscaunt')
-    .addGroupBy('d.Product_start_from') 
-    .addGroupBy('d.CashDiscount')
-    .addGroupBy('d.id')
-    .getRawMany()
+      .leftJoin('s.product', 'p')
+      .select('s.Total_stock', 'totalstock')
+      .where('p.id = :id', { id })
+      .getRawOne();
+    if (!FindStock) {
+      return {
+        message: 'No such product',
+        success: false,
+      };
+    }
+    if (FindStock.totalstock < productAmount) {
+      const product_status = StockStatus.NotEnough;
+      return {
+        message: 'Not enough product',
+        success: true,
+        data: { ...FindStock, product_status },
+      };
+    }
+    const product_status = StockStatus.Enough;
+    return {
+      message: 'sucessfuly  return',
+      success: true,
+      data: { ...FindStock, product_status },
+    };
+  };
+  CheckDiscountCalculate = async (
+    productId: number,
+    productAmount: number,
+  ): Promise<ResponseType<any>> => {
+    const checkDisc = await this.ProductDiscrepo.createQueryBuilder('d')
+      .leftJoin('d.product', 'p')
+      .select('d.percentageDiscaunt', 'percentageDiscaunt')
+      .addSelect('d.CashDiscount', 'CashDiscount')
+      .addSelect('d.Product_start_from', 'start_from')
+      .where('p.id = :productId', { productId })
+      .groupBy('p.id')
+      .addGroupBy('d.percentageDiscaunt')
+      .addGroupBy('d.Product_start_from')
+      .addGroupBy('d.CashDiscount')
+      .addGroupBy('d.id')
+      .getRawMany();
 
-    if(!checkDisc){
-      return{
-        message:"Failed to check  check discount info",
-        success:true
-      }
+    if (!checkDisc) {
+      return {
+        message: 'Failed to check  check discount info',
+        success: true,
+      };
     }
-    let matchDiscount = null
-    if(checkDisc.length === 0){
-      return{
-        message:"Product has no  discount",
-        success:true,
-        data:matchDiscount
-      }
+    let matchDiscount = null;
+    if (checkDisc.length === 0) {
+      return {
+        message: 'Product has no  discount',
+        success: true,
+        data: matchDiscount,
+      };
     }
-    
-    let discountAmount:any
-    const SortDisc = checkDisc.sort((a,b)=> a.start_from - b.start_from)
-    for(let i = 0; i < SortDisc.length; i++){
-      let currentDisc = SortDisc[i].start_from
-      
-      let nextDIsc = SortDisc[i +1]?SortDisc[i + 1].start_from: null
-      
-      if(productAmount >= currentDisc  && (!nextDIsc ||productAmount < nextDIsc )){
+
+    let discountAmount: any;
+    const SortDisc = checkDisc.sort((a, b) => a.start_from - b.start_from);
+    for (let i = 0; i < SortDisc.length; i++) {
+      let currentDisc = SortDisc[i].start_from;
+
+      let nextDIsc = SortDisc[i + 1] ? SortDisc[i + 1].start_from : null;
+
+      if (
+        productAmount >= currentDisc &&
+        (!nextDIsc || productAmount < nextDIsc)
+      ) {
         matchDiscount = currentDisc;
-        const filter_discont  = SortDisc.filter((item)=> item.start_from === matchDiscount)
-     return{
-        message:"Successfuly find discount ",
-        success:true,
-        data:{filter_discont}
-      }
+        const filter_discont = SortDisc.filter(
+          (item) => item.start_from === matchDiscount,
+        );
+        return {
+          message: 'Successfuly find discount ',
+          success: true,
+          data: { filter_discont },
+        };
       }
     }
-    
-    return{
-      message:"Successfuly",
-      success:true,
-      data:SortDisc
-      
-    }
-  }
 
+    return {
+      message: 'Successfuly',
+      success: true,
+      data: SortDisc,
+    };
+  };
+
+  CalculateDeviation = async (
+    input: DeviationInput,
+  ): Promise<ResponseType<any>> => {
+    const id = input.id;
+    const findSale_price = await this.ProductRepository.createQueryBuilder('p')
+      .select('p.wholesales_price', 'wholesales_price')
+      .addSelect('p.retailsales_price', 'retailsales_price')
+      .addSelect('p,rpurchase_price', 'rpurchase_price')
+      .addSelect('p.wpurchase_price', 'wpurchase_price')
+      .where('p.id = :id', { id })
+      .getRawOne();
+
+    if (!findSale_price) {
+      return {
+        message: 'No such product',
+        success: false,
+      };
+    }
+
+    const actualseling_price =
+      Number(findSale_price.wholesales_price) ??
+      Number(findSale_price.retailsales_price);
+    const bought_price =
+      findSale_price.wpurchase_price ?? findSale_price.rpurchase_price;
+    if (input.percentageDisc == null) {
+      const Exp_profit_pereach = actualseling_price - bought_price;
+      const Exp_Net_profit = Exp_profit_pereach * input.pnum;
+      const Net_profit = (input.sales - bought_price) * input.pnum;
+      const Profit_deviation = Exp_Net_profit - Net_profit;
+      const Expect_revenue = actualseling_price * input.pnum;
+      const Revenue = input.sales * input.pnum;
+      const deviationFromMeanPercent =
+        ((actualseling_price - input.sales) / Exp_profit_pereach) * 100;
+      return {
+        message: 'no',
+        success: true,
+        data: {
+          Revenue,
+          deviationFromMeanPercent,
+          Expect_revenue,
+          Exp_profit_pereach,
+          Exp_Net_profit,
+          Net_profit,
+          Profit_deviation,
+        },
+      };
+    } else {
+      if (input.CashDiscount !== null) {
+        const Act_sales = actualseling_price - input.CashDiscount;
+        const RequestSales_price = input.sales - input.CashDiscount;
+        const Exp_profit_pereach =
+          actualseling_price - bought_price - input.CashDiscount;
+        const Exp_Net_profit = Exp_profit_pereach * input.pnum;
+        const Net_profit =
+          (input.sales - bought_price - input.CashDiscount) * input.pnum;
+        const Profit_deviation = Exp_Net_profit - Net_profit;
+        const Expect_revenue =
+          (actualseling_price - input.CashDiscount) * input.pnum;
+        const Revenue = (input.sales - input.CashDiscount) * input.pnum;
+        const deviationFromMeanPercent =
+          ((Act_sales - RequestSales_price) / Exp_profit_pereach) * 100;
+        return {
+          message: 'yes',
+          success: true,
+          data: {
+            Revenue,
+            deviationFromMeanPercent,
+            Expect_revenue,
+            Exp_profit_pereach,
+            Exp_Net_profit,
+            Net_profit,
+            Profit_deviation,
+          },
+        };
+      }
+      const Exp_profit_pereach = actualseling_price - bought_price;
+      const Exp_Net_profit = Exp_profit_pereach * input.pnum;
+      const Net_profit = (input.sales - bought_price) * input.pnum;
+      const Profit_deviation = Exp_Net_profit - Net_profit;
+      const Expect_revenue = actualseling_price * input.pnum;
+      const Revenue = input.sales * input.pnum;
+      const deviationFromMeanPercent =
+        ((actualseling_price - input.sales) / Exp_profit_pereach) * 100;
+      return {
+        message: 'yes',
+        success: true,
+        data: {
+          Revenue,
+          deviationFromMeanPercent,
+          Expect_revenue,
+          Exp_profit_pereach,
+          Exp_Net_profit,
+          Net_profit,
+          Profit_deviation,
+        },
+      };
+    }
+  };
+
+  async SaleResponse(dto: SalesResponseDto): Promise<ResponseType<any>> {
+    const stock_check = await this.StockCheck(dto.ProductId, dto.Total_product);
+
+    const DiscontResult = await this.CheckDiscountCalculate(
+      dto.ProductId,
+      dto.Total_product,
+    );
  
-  CalculateDeviation = async( input: DeviationInput):Promise<ResponseType<any>> =>{
-     const id = input.id
-     const findSale_price = await this.ProductRepository.createQueryBuilder('p')
-     .select('p.wholesales_price', 'wholesales_price')
-     .addSelect('p.retailsales_price', 'retailsales_price')
-     .addSelect('p,rpurchase_price', 'rpurchase_price')
-     .addSelect('p.wpurchase_price', 'wpurchase_price')
-     .where('p.id = :id',{id})
-     .getRawOne()
+    const CalculateDeviation = await this.CalculateDeviation({
+      percentageDisc:
+        DiscontResult.data.filter_discont?.[0]?.percentageDiscaunt ?? null,
+      CashDiscount: DiscontResult.data.filter_discont?.[0].CashDiscount ?? null,
+      id: dto.ProductId,
+      sales: dto.Selling_price,
+      pnum: dto.Total_product,
+    });
 
-     if(!findSale_price){
-      return{
-        message:"No such product",
-        success:false
-      }
-     }
-
-    const actualseling_price =  Number(findSale_price.wholesales_price) ?? Number(findSale_price.retailsales_price)
-    const bought_price = findSale_price.wpurchase_price ?? findSale_price.rpurchase_price
-    if(input.percentageDisc == null){
-    const Exp_profit_pereach = actualseling_price - bought_price
-    const Exp_Net_profit = Exp_profit_pereach * input.pnum
-    const Net_profit = (input.sales - bought_price)*input.pnum;
-    const Profit_deviation = Exp_Net_profit -Net_profit
-    const Expect_revenue = actualseling_price * input.pnum
-    const  Revenue =  input.sales * input.pnum 
-    const deviationFromMeanPercent = ((actualseling_price-input.sales))/Exp_profit_pereach * 100
-    return{
-      message:"no",
-      success:true,
-      data:{ Revenue, deviationFromMeanPercent,Expect_revenue, Exp_profit_pereach, Exp_Net_profit, Net_profit, Profit_deviation}
-    }
-    }else{
-    if(input.CashDiscount !== null){
-    const Act_sales = actualseling_price - input.CashDiscount
-    const RequestSales_price  = input.sales - input.CashDiscount
-    const Exp_profit_pereach = (actualseling_price - bought_price)- input.CashDiscount
-    const Exp_Net_profit = (Exp_profit_pereach * input.pnum)
-    const Net_profit = (input.sales - bought_price -input.CashDiscount )*input.pnum;
-    const Profit_deviation = Exp_Net_profit - Net_profit
-    const Expect_revenue = (actualseling_price - input.CashDiscount) * input.pnum
-    const  Revenue =  (input.sales - input.CashDiscount) * input.pnum 
-    const deviationFromMeanPercent = ((Act_sales - RequestSales_price )/Exp_profit_pereach) * 100
-    return{
-      message:"yes",
-      success:true,
-      data:{ Revenue, deviationFromMeanPercent,Expect_revenue, Exp_profit_pereach, Exp_Net_profit, Net_profit, Profit_deviation}
-    }
-    }
-    const Exp_profit_pereach = (actualseling_price - bought_price)
-    const Exp_Net_profit = Exp_profit_pereach * input.pnum
-    const Net_profit = (input.sales - bought_price)*input.pnum;
-    const Profit_deviation = Exp_Net_profit -Net_profit
-    const Expect_revenue = actualseling_price * input.pnum
-    const  Revenue =  input.sales * input.pnum 
-    const deviationFromMeanPercent = ((actualseling_price-input.sales))/Exp_profit_pereach * 100
-    return{
-      message:"yes",
-      success:true,
-      data:{ Revenue, deviationFromMeanPercent,Expect_revenue, Exp_profit_pereach, Exp_Net_profit, Net_profit, Profit_deviation}
-    }
-    } 
- }
-
- async SaleResponse (dto:SalesResponseDto):Promise<ResponseType<any>>{
-  const stock_check =  await this.StockCheck(dto.ProductId,dto.Total_product)
-  // if(!stock_check.data.success){
-  //   return{
-  //     message:"stock fail",
-  //     success:false
-  //   }
-  // }
-  const DiscontResult = await this.CheckDiscountCalculate(dto.ProductId, dto.Total_product, )
-  // if(!DiscontResult.data.success){
-  //   return{
-  //     message:"Discount fail",
-  //     success:false
-  //   }
-  // }
-
-const CalculateDeviation = await this.CalculateDeviation({  
-  percentageDisc: DiscontResult.data.filter_discont?.[0]?.percentageDiscaunt ?? null,
-  CashDiscount:DiscontResult.data.filter_discont?.[0].CashDiscount ?? null,
-  id: dto.ProductId,
-  sales: dto.Selling_price,
-  pnum: dto.Total_product})
-  // if(!CalculateDeviation.data.success){
-  //   return{
-  //     message:"Deviation fail",
-  //     success:false
-  //   }
-  // }
-    return{
-    message:"successfuly",
-    success:true,
-    data:{stock_check, DiscontResult, CalculateDeviation}
+    return {
+      message: 'successfuly',
+      success: true,
+      data: { stock_check, DiscontResult, CalculateDeviation },
+    };
   }
-  
- }
-
-  }
-  
+}
