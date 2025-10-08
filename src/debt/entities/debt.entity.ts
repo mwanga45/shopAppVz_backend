0c3ae0f6b7466f9266b8cf4e638a99868fedd3be
mwanga45 <issamwanga02@gmail.com>
2025-10-08 22:28:46 +0300

    more changes

--- src/debt/entities/debt.entity.ts
+++ src/debt/entities/debt.entity.ts
@@ -1,44 +1,38 @@
-import { IsNumber, IsString } from "class-validator"
-import { User } from "src/entities/user.entity"
-import { Product } from "src/product/entities/product.entity"
-import { paymentstatus } from "src/type/type.interface"
-import { Column, JoinColumn, ManyToOne } from "typeorm"
+import { IsNumber, IsString } from 'class-validator';
+import { User } from 'src/entities/user.entity';
+import { Product } from 'src/product/entities/product.entity';
+import { paymentstatus } from 'src/type/type.interface';
+import { Column, JoinColumn, ManyToOne } from 'typeorm';
 
 export class Debt {
-      @Column()
-      @IsNumber()
-      Total_pc_pkg_litre:number
-    
-      @Column()
-      @IsString()
-      Revenue:number
-    
-      @Column()
-      @IsString()
-      Net_profit:number
-    
-      @Column()
-      Expected_Profit:number
-    
-      @Column('decimal', {precision:10, scale:4})
-      profit_deviation:number
-    
-      @Column()
-      percentage_deviation:number
-    
-      @Column()
-      percentage_discount:string
-    
-      @Column({default:paymentstatus.Paid})
-      paymentstatus:paymentstatus
+  @Column()
+  @IsNumber()
+  Total_pc_pkg_litre: number;
 
-      
-    
-    //   @ManyToOne(()=> User,(user)=>user.wholesales)
-    //   @JoinColumn({name:'userId'})
-    //   user:User;
-    
-    //   @ManyToOne(() => Product, (product)=> product.wholesales)
-    //   @JoinColumn({ name: 'productId' })
-    //   product: Product;
+  @Column()
+  @IsString()
+  Revenue: number;
+
+  @Column()
+  @IsString()
+  Net_profit: number;
+
+  @Column()
+  Expected_Profit: number;
+
+  @Column('decimal', { precision: 10, scale: 4 })
+  profit_deviation: number;
+
+  @Column()
+  percentage_deviation: number;
+
+  @Column()
+  percentage_discount: string;
+
+  @Column({ default: paymentstatus.Paid })
+  paymentstatus: paymentstatus;
+
+  @ManyToOne(() => User, (user) => user.debts)
+  @JoinColumn({ name: 'userId' })
+  user: User;
 }
