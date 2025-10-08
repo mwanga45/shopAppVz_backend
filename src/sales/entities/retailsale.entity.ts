87f89762b5b6aabb2322e4f8b1973b6796eb7009
mwanga45 <issamwanga02@gmail.com>
2025-10-08 10:42:35 +0300

    more changes

--- src/sales/entities/retailsale.entity.ts
+++ src/sales/entities/retailsale.entity.ts
@@ -1,45 +1,44 @@
-import { Entity ,Column} from "typeorm";
-import { baseEntity } from "src/common/base.entity";
-import { IsString } from "class-validator";
-import { Product } from "src/product/entities/product.entity";
-import { ManyToOne, JoinColumn } from "typeorm";
-import { User } from "src/entities/user.entity";
-import { paymentstatus } from "src/type/type.interface";
+import { Entity, Column } from 'typeorm';
+import { baseEntity } from 'src/common/base.entity';
+import { IsString } from 'class-validator';
+import { Product } from 'src/product/entities/product.entity';
+import { ManyToOne, JoinColumn } from 'typeorm';
+import { User } from 'src/entities/user.entity';
+import { paymentstatus } from 'src/type/type.interface';
 @Entity()
-export class RetailSales extends baseEntity{
-      @Column()
-      @IsString()
-      Total_pc_pkg_litre:string
-    
-      @Column()
-      @IsString()
-      Revenue:number
-    
-      @Column()
-      @IsString()
-      Net_profit:number
-    
-      @Column()
-      Expected_Profit:number
-    
-      @Column('decimal', {precision:10, scale:4})
-      profit_deviation:number
-    
-      @Column()
-      percentage_deviation:number
-    
-      @Column('decimal', {precision:10, scale:4 , default:0})
-      percentage_discount:number
-    
-      @Column({default:paymentstatus.Paid})
-      paymentstatus:paymentstatus
+export class RetailSales extends baseEntity {
+  @Column()
+  @IsString()
+  Total_pc_pkg_litre: string;
 
-    @ManyToOne(() => Product, { eager: true })
-    @JoinColumn({ name: 'productId' })
-    product: Product;
+  @Column()
+  @IsString()
+  Revenue: number;
 
-    @ManyToOne(()=> User,{eager:true})
-    @JoinColumn({name:'userId'})
-    user:User;
+  @Column()
+  @IsString()
+  Net_profit: number;
 
+  @Column()
+  Expected_Profit: number;
+
+  @Column('decimal', { precision: 10, scale: 4 })
+  profit_deviation: number;
+
+  @Column()
+  percentage_deviation: number;
+
+  @Column('decimal', { precision: 10, scale: 4, default: 0 })
+  percentage_discount: number;
+
+  @Column({ default: paymentstatus.Paid })
+  paymentstatus: paymentstatus;
+
+  @ManyToOne(() => Product, { eager: true })
+  @JoinColumn({ name: 'productId' })
+  product: Product;
+
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: 'userId' })
+  user: User;
 }
