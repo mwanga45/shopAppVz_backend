90c835d7dee9f64e326f8c53ca3fef2ce4cee9d6
mwanga45 <issamwanga02@gmail.com>
2025-10-10 11:19:00 +0300

    more changes

--- src/debt/debt.service.ts
+++ src/debt/debt.service.ts
@@ -9,6 +9,7 @@ import { DataSource, Repository } from 'typeorm';
 import { Product } from 'src/product/entities/product.entity';
 import { dialValidate } from 'src/common/helper/phone.helper';
 import { Customer } from 'src/entities/customer.entity';
+import { error } from 'console';
 
 @Injectable()
 export class DebtService {
@@ -21,73 +22,76 @@ export class DebtService {
     @InjectRepository(Customer)
     private readonly CustomerRepo: Repository<Customer>,
     private readonly dialservecheck: dialValidate,
-    private readonly DataSource:DataSource
+    private readonly DataSource: DataSource,
   ) {}
 
   async CreateDept(
     dto: CreateDebtDto,
     userId: any,
   ): Promise<ResponseType<any>> {
-    return await this.DataSource.transaction(async (manager)=>{
-    try{
-    const findproduct = await manager.findOne(Product,{
-      where: { id: dto.ProductId },
-    });
-    if(!findproduct) throw new Error('Product is not Eixts')
+    return await this.DataSource.transaction(async (manager) => {
+      try {
+        const findproduct = await manager.findOne(Product, {
+          where: { id: dto.ProductId },
+        });
+        if (!findproduct) throw new Error('Product is not Eixts');
 
-    const isValidPh_number = this.dialservecheck.CheckDialformat(
-      dto.Phone_number,
-    );
-    if (!isValidPh_number.success) throw new Error(String(isValidPh_number.message))
-    const checkUserphone_exist = await manager.findOne(Customer,{
-      where: { phone_number: isValidPh_number.data },
-    });
+        const isValidPh_number = this.dialservecheck.CheckDialformat(
+          dto.Phone_number,
+        );
+        if (!isValidPh_number.success)
+          throw new Error(String(isValidPh_number.message));
+        const checkUserphone_exist = await manager.findOne(Customer, {
+          where: { phone_number: isValidPh_number.data },
+        });
 
-    if (!checkUserphone_exist) {
-      const addCustomer = manager.create(Customer,{
-        customer_name: dto.Debtor_name,
-        phone_number: isValidPh_number.data,
-        Location: dto.location || 'none',
-      });
-      await manager.save(addCustomer);
-    }
-    const AddDebt = manager.create(Debt,{
-      product: { id: dto.ProductId },
-      paidmoney: dto.Paid,
-      debtname: dto.Debtor_name,
-      Net_profit: dto.Net_profit,
-      Expected_Profit: dto.Expecte_profit,
-      phone_number: dto.Phone_number,
-      Revenue: dto.Revenue,
-      percentage_deviation: dto.Percentage_deviation,
-      profit_deviation: dto.profit_deviation,
-      Total_pc_pkg_litre: dto.Total_pc_pkg_litre,
-      percentage_discount: dto.Discount_percentage,
-      paymentstatus: dto.paymentstatus,
-      PaymentDateAt:dto.PaymentDateAt,
-      user: { id: userId },
-    });
-    const saveDebt = await manager.save(AddDebt);
-    if (!saveDebt || !saveDebt.id) throw new Error('Failed to add record please try again')
-    const Addtrack = manager.create(Debt_track,{
-      paidmoney: dto.Paid,
-      debt: { id: saveDebt.id },
-      user: { id: userId },
-    });
+        if (!checkUserphone_exist) {
+          const addCustomer = manager.create(Customer, {
+            customer_name: dto.Debtor_name,
+            phone_number: isValidPh_number.data,
+            Location: dto.location || 'none',
+          });
+          await manager.save(addCustomer);
+        }
+        const AddDebt = manager.create(Debt, {
+          product: { id: dto.ProductId },
+          paidmoney: dto.Paid,
+          debtname: dto.Debtor_name,
+          Net_profit: dto.Net_profit,
+          Expected_Profit: dto.Expecte_profit,
+          phone_number: dto.Phone_number,
+          Revenue: dto.Revenue,
+          percentage_deviation: dto.Percentage_deviation,
+          profit_deviation: dto.profit_deviation,
+          Total_pc_pkg_litre: dto.Total_pc_pkg_litre,
+          percentage_discount: dto.Discount_percentage,
+          paymentstatus: dto.paymentstatus,
+          PaymentDateAt: dto.PaymentDateAt,
+          user: { id: userId },
+        });
+        const saveDebt = await manager.save(AddDebt);
+        if (!saveDebt || !saveDebt.id)
+          throw new Error('Failed to add record please try again');
+        const Addtrack = manager.create(Debt_track, {
+          paidmoney: dto.Paid,
+          debt: { id: saveDebt.id },
+          user: { id: userId },
+        });
 
-    const savedTrack = await manager.save(Addtrack);
-    if (!savedTrack || !savedTrack.id) throw new Error('failed to add track')
-    return {
-      message: 'successfuly Add and make followup data',
-      success: true,
-    };
-  }catch(error){
-    return{
-      message:`Transaction failed: ${error.message}`,
-      success:false
-    }
-  }
-       })
+        const savedTrack = await manager.save(Addtrack);
+        if (!savedTrack || !savedTrack.id)
+          throw new Error('failed to add track');
+        return {
+          message: 'successfuly Add and make followup data',
+          success: true,
+        };
+      } catch (error) {
+        return {
+          message: `Transaction failed: ${error.message}`,
+          success: false,
+        };
+      }
+    });
   }
 
   async UpdateDebt(
@@ -95,59 +99,54 @@ export class DebtService {
     userId: any,
     id: any,
   ): Promise<ResponseType<any>> {
-    return this.DataSource.transaction(async(manager)=>{
-    try{
-    const findDebt = await manager.findOne(Debt,{
-      where: { id:id },
-    });
-    if (!findDebt) throw  new Error('Debt data is not exist')
-  
-    if (
-      findDebt.paidmoney === findDebt.Revenue ||
-      findDebt.paymentstatus === paymentstatus.Paid
-    ) {
-      if (findDebt.paymentstatus !== paymentstatus.Paid) {
-        const checkpayementstatus = await manager.update(Debt,
-          { id:id},
-          { paymentstatus: paymentstatus.Paid },
-        );
-        if (
-          !checkpayementstatus.affected ||
-          checkpayementstatus.affected === 0
-        ) throw new Error( 'failed to update  paymentstatus')
-      }
-      throw new Error('The debt is already  been completed paid')
-    }
-    const UpdateDebt = await this.DebtRepo.update({ id: id }, dto);
+    return this.DataSource.transaction(async (manager) => {
+      try {
+        const findDebt = await manager.findOne(Debt, {
+          where: { id: id },
+        });
+        if (!findDebt) throw new Error('Debt data is not exist');
 
-    if (!UpdateDebt.affected || UpdateDebt.affected === 0) {
-      return {
-        message: 'failed to make update',
-        success: false,
-      };
-    }
-    const AddDebtTrack = this.DebtTrackRepo.create({
-      debt: { id: id },
-      paidmoney: dto.Paid,
-      user: { id: userId },
-    });
-    const savedTrack = await this.DebtTrackRepo.save(AddDebtTrack);
-    if (!savedTrack || !savedTrack.id) {
-      return {
-        message: 'Failed to addtrack',
-        success: false,
-      };
-    }
-    return {
-      message: 'successfuly  update debt',
-      success: true,
-    };
-        }catch(error){
-          return{
-            message:`Transaction failed: ${error.message}`,
-            success:false
+        if (
+          findDebt.paidmoney === findDebt.Revenue ||
+          findDebt.paymentstatus === paymentstatus.Paid
+        ) {
+          if (findDebt.paymentstatus !== paymentstatus.Paid) {
+            const checkpayementstatus = await manager.update(
+              Debt,
+              { id: id },
+              { paymentstatus: paymentstatus.Paid },
+            );
+            if (
+              !checkpayementstatus.affected ||
+              checkpayementstatus.affected === 0
+            )
+              throw new Error('failed to update  paymentstatus');
           }
+          throw new Error('The debt is already  been completed paid');
         }
-  })
+        const UpdateDebt = await manager.update(Debt, { id: id }, dto);
+
+        if (!UpdateDebt.affected || UpdateDebt.affected === 0)
+          throw new Error('failed to make update');
+        const AddDebtTrack = manager.create(Debt_track, {
+          debt: { id: id },
+          paidmoney: dto.Paid,
+          user: { id: userId },
+        });
+        const savedTrack = await manager.save(AddDebtTrack);
+        if (!savedTrack || !savedTrack.id)
+          throw new Error('Failed to addtrack');
+
+        return {
+          message: 'successfuly  update debt',
+          success: true,
+        };
+      } catch (error) {
+        return {
+          message: `Transaction failed: ${error.message}`,
+          success: false,
+        };
+      }
+    });
   }
 }
