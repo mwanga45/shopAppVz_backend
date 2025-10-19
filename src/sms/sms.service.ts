f36965e58a0c1cc366f9e1ad2a5296eaebb57e32
mwanga45 <issamwanga02@gmail.com>
2025-10-19 17:44:42 +0300

    more changes

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -2,9 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { CreateSmDto } from './dto/create-sm.dto';
 import { UpdateSmDto } from './dto/update-sm.dto';
 import { ResponseType , SmsPayload} from 'src/type/type.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SmsService {
+  constructor(
+    private configservice:ConfigService
+  ){}
   async SendSms(createSmDto: CreateSmDto):Promise<ResponseType<any>> {
 
    const Payload: SmsPayload ={
@@ -12,8 +16,19 @@ export class SmsService {
     sms:createSmDto.sms,
     recipients:[{number:createSmDto.phone_number}],
     schedule:"none"
-
    }
+  const  url  = this.configservice.get<string>('BASE_URL') || ''
+  const token = this.configservice.get<string>('SMS_TOKEN')
+
+const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(Payload),
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
+    },
+})
     return{
       message:"successfuly sent",
       success:true
