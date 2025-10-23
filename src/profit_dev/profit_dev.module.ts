16fd58e939b109a85f8ab0e6ccdb0a26ecc1a6ed
mwanga45 <issamwanga02@gmail.com>
2025-10-23 10:50:17 +0300

    more chnages

--- src/profit_dev/profit_dev.module.ts
+++ src/profit_dev/profit_dev.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DailyProfitsummary } from 'src/sales/entities/profitsummary.entity';
+import { ProfitDevService } from './profit_dev.service';
+import { ProfitDevController } from './profit_dev.controller';
 
 @Module({
     imports:[
         TypeOrmModule.forFeature([DailyProfitsummary])
-    ]
+    ],
+    providers:[ProfitDevService],
+    controllers:[ProfitDevController]
+
 })
 export class ProfitDevModule {}
--- src/profit_dev/profit_dev.service.ts
+++ src/profit_dev/profit_dev.service.ts
@@ -6,23 +6,23 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProfitDevService {
-    constructor(
-        @InjectRepository(DailyProfitsummary)
-        private readonly ProfitsummaryRepo:Repository<DailyProfitsummary>
-    ){}
+  constructor(
+    @InjectRepository(DailyProfitsummary)
+    private readonly ProfitsummaryRepo: Repository<DailyProfitsummary>,
+  ) {}
 
-    async AdminAnalysis ():Promise<ResponseType<any>>{
-        const now = new Date()
-        const dateOfToday = now.toISOString().split('T')[0];
-        const profit = await this.ProfitsummaryRepo.createQueryBuilder('d')
-        .select('d.total_profit')
-        .orderBy('d.id', 'DESC')
-        .limit (2)
-        .getRawMany()
-        return{
-            message:"successly returned",
-            success:true,
-            data:profit
-        }
-    }
+  async AdminAnalysis(): Promise<ResponseType<any>> {
+    const now = new Date();
+    const dateOfToday = now.toISOString().split('T')[0];
+    const profit = await this.ProfitsummaryRepo.createQueryBuilder('d')
+      .select('d.total_profit')
+      .orderBy('d.id', 'DESC')
+      .limit(2)
+      .getRawMany();
+    return {
+      message: 'successly returned',
+      success: true,
+      data: profit,
+    };
+  }
 }
