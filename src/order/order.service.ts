0681eccfb395aa9b529ee829e2c78aac038f98b8
mwanga45 <issamwanga02@gmail.com>
2025-10-24 16:15:55 +0300

    more changes

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -10,6 +10,7 @@ import { Customer } from 'src/entities/customer.entity';
 import { category, ResponseType } from 'src/type/type.interface';
 import { Product } from 'src/product/entities/product.entity';
 import { UnofficialProduct } from './entities/Unofficialproduct.entity';
+import { DataSource } from 'typeorm';
 
 
 
@@ -23,86 +24,33 @@ export class OrderService {
     private readonly  ProductRepo:Repository<Product>,
     @InjectRepository(UnofficialProduct)
     private readonly  UproductRepo:Repository<UnofficialProduct>,
+    private readonly Datasource:DataSource,
     private readonly validator:dialValidate
   ){}
   private ordertype:Ordertype[] = [] 
+ 
   async createOrder(dto:CreateOrderDto) :Promise<ResponseType<any>>{
-  //    const dial_number = this.validator.CheckDialformat(dto.client_phone)
-     
-  //   const create =  this.orderRepo.create({
-  //     product_name:dto.product_name,
-  //     userId:dto.user_id,
-  //     client_name:dto.client_name,
-  //     client_phone:dial_number.data,
-  //     OrderDate:dto.OrderDate
-  //   })
-  //   return this.orderRepo.save(create)
-  // }
+ return await this.Datasource.transaction(async(manager)=> {
+    try{
 
-  // async ProductSummary():Promise<{
-  //   ActiveOrder:{
-  //     data:any[];
-  //     count:number;
-  //     message?:any;
-  //   },
-  //   NearlyOrder:{
-  //     data:any[];
-  //     count:number;
-  //     message?:string;
-  //   },
-  //   OrdersHistory:{
-  //     data:any[];
-  //     count:number;
-  //     message?: string;
-  //   }
-  // }>{
-  //   const now = new Date();
-  //   const dateOfToday = [
-  //     String(now.getDate()).padStart(2,'0'),
-  //     String(now.getMonth()+1).padStart(2,'0'),
-  //     String(now.getFullYear()),
-  //   ].join('/')
+       return{
+        message:"successfuly",
+        success:true
+      }
 
-  //   const FeatureDay = [
-  //     String(now.getDate()+2).padStart(2,'0'),
-  //     String(now.getMonth()+1).padStart(2,'0'),
-  //     String(now.getFullYear())
-  //   ].join("/")
-  //  console.log("feature ",FeatureDay)
-  //  console.log("dateof to day",dateOfToday)
-  //  const [OrderHistory,ActiveOrders,NearlyOrders]=  await Promise.all([
-  //   this.orderRepo.find(),
-  //   this.orderRepo.find({where:{OrderDate:LessThanOrEqual(dateOfToday)}}),
-  //   this.orderRepo.find({where:{OrderDate:MoreThanOrEqual(FeatureDay)}})
-  //  ])
-  //  return {
-  //    ActiveOrder:{
-  //     data:ActiveOrders,
-  //     count:ActiveOrders.length,
-  //     ...(ActiveOrders.length === 0 &&{message:"No Active Order Available"})
-      
-  //   },
-  //   OrdersHistory:{
-  //     data:OrderHistory,
-  //     count:OrderHistory.length,
-  //      ...(OrderHistory.length === 0 && {message:"No Order History found at all"})  
-  //   },
-  //   NearlyOrder:{
-  //     data:NearlyOrders,
-  //     count:NearlyOrders.length,
-  //     ...(NearlyOrders.length === 0 && {message:"No Order in near feature"})
-  //   }
-
-  // }
+    }catch(error){
+      return{
+        message:"su",
+        success:false
+      }
+    }
+ })
+  }
   // create an  function  that  will Automatic make sure is cancelled after ten days 
   // after status stays pending for that time and then send are message  as record of 
   // automatic  cancellation of order(Automatic invoke functu=ion) 
-  return{
-    message:"su",
-    success:true
-  }
 
-  }
+  
   async findAllcustomer():Promise<ResponseType<any>>{
   const customerdetails = await this.CustomerRepo.createQueryBuilder('c')
   .select([
