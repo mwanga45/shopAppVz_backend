339aa29e29ec23e0055cf2545d2f61d95f3eebcb
mwanga45 <issamwanga02@gmail.com>
2025-11-08 09:54:06 +0300

    more changes

--- src/entities/cashFlow.entity.ts
+++ src/entities/cashFlow.entity.ts
@@ -3,18 +3,21 @@ import { Column, Entity } from "typeorm";
 
 @Entity()
 export class CashFlow extends baseEntity{
-  @Column({ type: "decimal", precision: 15, scale: 2, default: 0 })
-  bankCash: number;
+  // @Column({ type: "decimal", precision: 15, scale: 2, default: 0 })
+  // bankCash: number;
 
-  @Column({ type: "decimal", precision: 15, scale: 2, default: 0 })
-  onHandCash: number;
+  // @Column({ type: "decimal", precision: 15, scale: 2, default: 0 })
+  // onHandCash: number;
 
   @Column({ type: "decimal", precision: 15, scale: 2, default: 0 })
   bankDebt: number;
 
-  @Column({type:'decimal', precision:10, scale:2, default:0})
+  @Column({type:'decimal', precision:15, scale:2, default:0})
   Electricity:number
+  
+  @Column({type:'decimal', precision:15, scale:2, default:0})
+  Salary:number
 
-  @Column({type:"decimal", precision:10, scale:2})
+  @Column({type:"decimal", precision:15, scale:2})
   Food:number
 }
