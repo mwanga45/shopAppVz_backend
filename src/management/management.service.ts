78cff48709326633a7589303fd3c4d11d2643292
mwanga45 <issamwanga02@gmail.com>
2025-11-15 10:48:17 +0300

    more changes

--- src/management/management.service.ts
+++ src/management/management.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateManagementDto } from './dto/create-management.dto';
 import { UpdateManagementDto } from './dto/update-management.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Capital } from 'src/entities/capital.entity';
+import { CashFlow } from 'src/entities/cashFlow.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ManagementService {
+  constructor(
+    @InjectRepository(Capital)
+    private readonly Capitalrepo: Repository<Capital>,
+    @InjectRepository(CashFlow)
+    private readonly CashflowRepo: Repository<CashFlow>,
+  ) {}
   create(createManagementDto: CreateManagementDto) {
     return 'This action adds a new management';
   }
