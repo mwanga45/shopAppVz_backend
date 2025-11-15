b5deb0c999306ff9c82fc74f1253b68d6de47280
mwanga45 <issamwanga02@gmail.com>
2025-11-15 11:17:54 +0300

    more changes

--- src/management/management.module.ts
+++ src/management/management.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ManagementService } from './management.service';
 import { ManagementController } from './management.controller';
+import { Capital } from 'src/entities/capital.entity';
+import { CashFlow } from 'src/entities/cashFlow.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports:[
+   TypeOrmModule.forFeature([Capital, CashFlow])
+  ],
   controllers: [ManagementController],
   providers: [ManagementService],
 })
