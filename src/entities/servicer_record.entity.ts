f185382f8e82176578e32ac3572affc7b1fe013d
mwanga45 <issamwanga02@gmail.com>
2025-11-17 12:35:49 +0300

    more changes

--- src/entities/servicer_record.entity.ts
+++ src/entities/servicer_record.entity.ts
@@ -1,7 +1,15 @@
 import { baseEntity } from "src/common/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity } from "typeorm";
 
 @Entity()
 export class serviceRecord extends baseEntity{
-    
+@Column()
+service_name:string
+
+@Column()
+icon_name:string
+
+@Column({type:'decimal', precision:15, scale:2})
+price:number
+
 }
