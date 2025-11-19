b6ac80f6f2827a660c14210141fd6711ee0e443c
mwanga45 <issamwanga02@gmail.com>
2025-11-19 22:59:07 +0300

    more changes

--- src/common/helper/rate.helper.ts
+++ src/common/helper/rate.helper.ts
@@ -1,8 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Res } from "@nestjs/common";
+import { ResponseType } from "src/type/type.interface";
 
 @Injectable()
 export class BusinessGrowthLogic {
-  RateCalculation (){
-    
+  RateCalculation (firstdata:number, secondData:number):ResponseType<any>{
+   let rate_status:string
+   let rate:number
+   let Result:any
+
+   if(firstdata > secondData){
+    rate_status = 'up'
+    rate = (firstdata - secondData)/(firstdata + secondData) * 100
+    Result = {rate, rate_status}
+    return Result
+   }else if(secondData - firstdata){
+    rate_status = 'down'
+    rate = (secondData - firstdata)/(secondData + firstdata) * 100
+    Result
+    return Result 
+   }else{
+    rate = 0
+    rate_status ='equal'
+    Result ={rate, rate_status}
+    return Result
+   }
+  
   }
 }
