225d99de92341b9a5749d37a046ad7fd8a170a90
mwanga45 <issamwanga02@gmail.com>
2025-11-19 23:01:23 +0300

    more changes

--- src/common/helper/rate.helper.ts
+++ src/common/helper/rate.helper.ts
@@ -1,29 +1,34 @@
-import { Injectable, Res } from "@nestjs/common";
-import { ResponseType } from "src/type/type.interface";
+import { Injectable, Res } from '@nestjs/common';
+import { ResponseType } from 'src/type/type.interface';
 
 @Injectable()
 export class BusinessGrowthLogic {
-  RateCalculation (firstdata:number, secondData:number):ResponseType<any>{
-   let rate_status:string
-   let rate:number
-   let Result:any
+  RateCalculation(firstdata: number, secondData: number): ResponseType<any> {
+    let rate_status: string;
+    let rate: number;
+    let Result: any;
 
-   if(firstdata > secondData){
-    rate_status = 'up'
-    rate = (firstdata - secondData)/(firstdata + secondData) * 100
-    Result = {rate, rate_status}
-    return Result
-   }else if(secondData - firstdata){
-    rate_status = 'down'
-    rate = (secondData - firstdata)/(secondData + firstdata) * 100
-    Result
-    return Result 
-   }else{
-    rate = 0
-    rate_status ='equal'
-    Result ={rate, rate_status}
-    return Result
-   }
-  
+    if (firstdata > secondData) {
+      rate_status = 'up';
+      rate = ((firstdata - secondData) / (firstdata + secondData)) * 100;
+      Result = { rate, rate_status };
+      
+    } else if (secondData - firstdata) {
+      rate_status = 'down';
+      rate = ((secondData - firstdata) / (secondData + firstdata)) * 100;
+      Result;
+    
+    } else {
+      rate = 0;
+      rate_status = 'equal';
+      Result = { rate, rate_status };
+    }
+    Result = { rate , rate_status}
+    return{
+        message:"successfuly returned",
+        success:true, 
+        data:Result
+    }
+
   }
 }
